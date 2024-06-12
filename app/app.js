2c69795a6e02ee63d5e782e3686acb5f6119e22c
birender <birenderk935@gmail.com>
2024-06-12 15:54:10 +0530

    cors7

--- app/app.js
+++ app/app.js
@@ -19,7 +19,7 @@ const adminRoute = require("../routes/auth/adminRoute");
 const blogRouter = require("../routes/blog/blogRouter");
 const adminDashAnalysisRouter = require("../routes/admin-dashboard-analysis/adminDashAnalysisRoute");
 const app = express();
-
+app.use(cors())
 
 // cors options
 const whitelist = ["https://ecommerce-api-green.vercel.app"];
@@ -45,9 +45,9 @@ app.use(express.urlencoded({extended:false})); // upload image
 
 
 //cors all routes
-app.use(cors({
-  origin:" * ",
-}));
+// app.use(cors({
+//   origin:" * ",
+// }));
 
 
 //Routes
