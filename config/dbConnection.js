1f7ebaf0f61c0b81c3fdb8ca8639467a0dbd941f
birender <birenderk935@gmail.com>
2024-06-12 12:46:53 +0530

    add database file

---
+++ config/dbConnection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery',false);
+
+const dbConnect = async() => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL,{useUnifiedTopology: true,useNewUrlParser: true,});
+      console.log("DB Connected Successfully")
+  } catch (error) {
+    console.log("DB Connected Failed --> ", error)
+  }  
+};
+
+dbConnect();
+
+
+
+
